64f88975ed37b2bd09aee7d7dadeea023e3aee4c
gabrielgs44 <gabriel.gomes.silva.999@gmail.com>
2019-10-14 16:13:20 -0300

    Estudo c#, aprendendo delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using Delegates.Services;
+using System;
+
+namespace Delegates
+{
+
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op(a, b);
+        }
+    }
+}
